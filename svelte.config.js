92c0886a550e5505d6f65ffaec24ae82b0f92f9b
The Whiteroom <rivalisthrowaway@gmail.com>
2024-05-19 03:55:42 +1200

    message

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,9 @@ import { mdsvex } from 'mdsvex';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	},
+		appDir: "app",
+		adapter: adapter()
+	  },
 
 	extensions: ['.svelte', '.md'],
 
