d18ade2f2a472a359b8ef0e8f25d5465f54f5303
The Whiteroom <rivalisthrowaway@gmail.com>
2024-05-19 04:34:09 +1200

    remove kit

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,7 @@ import { mdsvex } from 'mdsvex';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter(),
 	},
 
 	extensions: ['.svelte', '.md'],
